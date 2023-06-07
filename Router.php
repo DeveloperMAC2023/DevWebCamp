ee10b5ebd28186424dd7a2c7f666c2d1ac7a14fe
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-06-07 19:08:38 -0400

    Finalizadas las ramas de Autenticacion y Creando Header

--- Router.php
+++ Router.php
@@ -19,16 +19,15 @@ class Router
 
     public function comprobarRutas()
     {
-
+        
         $url_actual = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
-
+        
         if ($method === 'GET') {
             $fn = $this->getRoutes[$url_actual] ?? null;
         } else {
             $fn = $this->postRoutes[$url_actual] ?? null;
         }
-
         if ( $fn ) {
             call_user_func($fn, $this);
         } else {
