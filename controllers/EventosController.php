daafe0e69a88524fefc017c281697c8bba9907bf
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-06-15 18:44:13 -0400

    Finalizada rama WorkShops

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -3,6 +3,10 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Categoria;
+use Model\Dia;
+use Model\Evento;
+use Model\Hora;
 
 class EventosController {
 
@@ -15,10 +19,30 @@ class EventosController {
     public static function crear(Router $router) {
 
         $alertas = [];
+        $categorias = Categoria::all('ASC');
+        $dias = Dia::all('ASC');
+        $horas = Hora::all('ASC');
+
+        $evento = new Evento;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $evento->sincronizar($_POST);
+            $alertas = $evento->validar();
+            if (empty($alertas)) {
+                $resultado = $evento->guardar();
+                if($resultado) {
+                    header('Location: /admin/eventos');
+                }
+            }
+        }
 
         $router->render('admin/eventos/crear', [
             'titulo' => 'Registrar Evento',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'categorias' => $categorias,
+            'dias' => $dias,
+            'horas' => $horas,
+            'evento' => $evento
         ]);
     }
 
