c534de63ce39b3d470be0d3e26e146333e18ee87
DeveloperMAC2023 <DeveloperMAC2023@gmail.com>
2023-06-24 14:20:22 -0400

    Finalizada rama Principal

---
+++ src/js/mapa.js
@@ -0,0 +1,19 @@
+if(document.querySelector('#mapa'))  {
+
+    const lat = -33.455928 
+    const lng = -70.649696
+    const zoom = 16
+
+    var map = L.map('mapa').setView([lat, lng], zoom);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup(`
+            <h2 class="mapa__heading">DevWebCamp</h2>
+            <p class="mapa__texto">Centro de Convenciones Santiago de Chile</p>
+        `)
+        .openPopup();
+}
